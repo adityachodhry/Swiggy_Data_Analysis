82bd3e45e1b5802240af2bdcac596d55cfe7444c
adityachodhry <adityachy02131@gmail.com>
2025-03-08 14:27:26 +0530

    For Swiggy Data Analysis

--- cityWiseRestaurant.py
+++ cityWiseRestaurant.py
@@ -3,50 +3,52 @@ import json
 from city_lat_lon import get_lat_lon  
 
 
-cityName = "Indore"
-restaurantName = "Gurukripa Restaurant - Sarwate"
+def primaryRestaurantId (latitude, longitude, primaryRestaurantId):
 
-latitude, longitude = get_lat_lon(cityName)
+    cityName = "Indore"
+    restaurantName = "Gurukripa Restaurant - Sarwate"
 
-url = f"https://www.swiggy.com/dapi/restaurants/search/suggest?lat={latitude}&lng={longitude}&str={restaurantName.replace(' ', '%20')}&trackingId=undefined&includeIMItem=true"
+    latitude, longitude = get_lat_lon(cityName)
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
-}
+    url = f"https://www.swiggy.com/dapi/restaurants/search/suggest?lat={latitude}&lng={longitude}&str={restaurantName.replace(' ', '%20')}&trackingId=undefined&includeIMItem=true"
 
-response = requests.get(url, headers=headers)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+    }
 
-if response.status_code == 200:
-    data = response.json() 
+    response = requests.get(url, headers=headers)
 
-    with open('cityRaw.json', 'w', encoding='utf-8') as json_file:
-        json.dump(data, json_file, indent=4, ensure_ascii=False)
+    if response.status_code == 200:
+        data = response.json() 
 
-    print("Data saved to cityRaw.json successfully!")
+        with open('cityRaw.json', 'w', encoding='utf-8') as json_file:
+            json.dump(data, json_file, indent=4, ensure_ascii=False)
 
-    restaurantIdDetails = data.get('data', {}).get('suggestions', [])
+        print("Data saved to cityRaw.json successfully!")
 
-    if restaurantIdDetails:
-        first_restaurant = restaurantIdDetails[0]  
-        metadata_str = first_restaurant.get('metadata')
+        restaurantIdDetails = data.get('data', {}).get('suggestions', [])
 
-        if metadata_str:
-            try:
-                metadata = json.loads(metadata_str)
-                primaryRestaurantId = metadata.get("data", {}).get("primaryRestaurantId")
+        if restaurantIdDetails:
+            first_restaurant = restaurantIdDetails[0]  
+            metadata_str = first_restaurant.get('metadata')
 
-                if primaryRestaurantId:
-                    print(f"Primary Restaurant ID (First Entry): {primaryRestaurantId}")
-                else:
-                    print("Primary Restaurant ID not found in metadata.")
+            if metadata_str:
+                try:
+                    metadata = json.loads(metadata_str)
+                    primaryRestaurantId = metadata.get("data", {}).get("primaryRestaurantId")
 
-            except json.JSONDecodeError:
-                print("Error: Could not decode metadata JSON.")
+                    if primaryRestaurantId:
+                        print(f"Primary Restaurant ID (First Entry): {primaryRestaurantId}")
+                    else:
+                        print("Primary Restaurant ID not found in metadata.")
+
+                except json.JSONDecodeError:
+                    print("Error: Could not decode metadata JSON.")
+            else:
+                print("No metadata found for the first restaurant.")
         else:
-            print("No metadata found for the first restaurant.")
-    else:
-        print("No restaurant suggestions found.")
+            print("No restaurant suggestions found.")
 
-else:
-    print(f"Error: HTTP {response.status_code} - {response.text}")
+    else:
+        print(f"Error: HTTP {response.status_code} - {response.text}")
 
--- swiggy_menu.py
+++ swiggy_menu.py
@@ -1,6 +1,8 @@
 import requests
 import json
 
+
+
 url = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.7195687&lng=75.8577258&restaurantId=84070&catalog_qa=undefined&submitAction=ENTER"
 
 
